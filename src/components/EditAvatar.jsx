dfcefbe18f400067ff966694ec88f383ef043636
EduardoTrevizo <Edwardo_tp@hotmail.com>
2024-11-19 16:58:25 -0600

    correccion ubicacion de archivos editavatar,newcard,popup

--- src/components/EditAvatar.jsx
+++ src/components/EditAvatar.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import PopupWithForm from "../../../PopupWithForm";
+import PopupWithForm from "./PopupWithForm";
 import { useState } from "react";
 
 export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
