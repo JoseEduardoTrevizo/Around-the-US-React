f25857f976c534cd58333b10e22444badccd3863
EduardoTrevizo <Edwardo_tp@hotmail.com>
2024-11-20 19:49:39 -0600

    ubicacion de archivos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import Header from "./Header/Header";
 import Main from "./Main/Main";
-import Footer from "./footer/Footer";
-import EditProfile from "./Main/components/Popup/EditProfile";
-import EditAvatarPopup from "./Main/components/Popup/EditAvatar";
-import NewCardPopup from "./Main/components/Popup/NewCard";
-import ConfirmationPopup from "./Main/components/Popup/RemoveCard";
+import Footer from "./Footer/Footer";
+import EditProfile from "./Main/components/Popup/EditProfile/EditProfile";
+import EditAvatarPopup from "./Main/components/Popup/EditAvatar/EditAvatar";
+import NewCardPopup from "../components/Main/components/Popup/NewCard/NewCard";
+import ConfirmationPopup from "../components/Main/components/Popup/RemoveCard/RemoveCard";
 import { useState, useEffect } from "react";
 import CurrentUserContext from "../contexts/CurrentUserContext";
 import api from "../utils/api";
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import ImageVector from "../../images/Vector.png";
 import ImageEdit from "../../images/Edit_Button.png";
 import ImageAdd from "../../images/Add_Button.png";
-import ImagePopup from "./components/Popup/ImagePopup";
+import ImagePopup from "../Main/components/Popup/ImagePopup/ImagePopup";
 import { useContext } from "react";
 import Card from "./components/Card/Card";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
